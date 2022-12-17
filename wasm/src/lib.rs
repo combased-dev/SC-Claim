e6135994fdd3548bb57001c025a9f69990592b71
LightAssassin89 <99542515+LightAssassin89@users.noreply.github.com>
2022-12-17 19:47:14 +0000

    Add files via upload

---
+++ wasm/src/lib.rs
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////
+////////////////// AUTO-GENERATED //////////////////
+////////////////////////////////////////////////////
+
+#![no_std]
+
+elrond_wasm_node::wasm_endpoints! {
+    claim
+    (
+        change_timer
+        change_token_id
+        change_token_nonce
+        claim
+        fund
+        getDeadline
+        getNonce
+        getNonceAlreadyClaimed
+        getNonceId
+        getRewardPerNft
+        getStatus
+        getTimer
+        getTokenId
+        getTokenIdNft
+        gtNftInCollection
+        start_claim
+        stop_claim
+        withdraw
+    )
+}
+
+elrond_wasm_node::wasm_empty_callback! {}
